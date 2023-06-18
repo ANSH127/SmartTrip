d9f9d737de3025e82c5334f06d7c1649121889c6
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-18 23:20:46 +0530

    updating files

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -2,10 +2,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
 import AddTripScreen from '../screens/AddTripScreen';
 import TripExpensesScreen from '../screens/TripExpensesScreen';
 import AddExpenseScreen from '../screens/AddExpenseScreen';
+import WelcomScreen from '../screens/WelcomScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,9 +15,11 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'  >
+      <Stack.Navigator initialRouteName='Welcom'  >
+        <Stack.Screen options={{ headerShown: false}} name="Welcom" component={WelcomScreen} />
         <Stack.Screen options={{ headerShown: false}} name="Home" component={HomeScreen} />
-        <Stack.Screen options={{ headerShown: false}} name="login" component={LoginScreen} />
+        <Stack.Screen options={{ headerShown: false,presentation:'modal'}} name="SignIn" component={SignInScreen} />
+        <Stack.Screen options={{ headerShown: false,presentation:'modal'}} name="SignUp" component={SignUpScreen} />
         <Stack.Screen options={{ headerShown: false}} name="AddTrip" component={AddTripScreen} />
         <Stack.Screen options={{ headerShown: false}} name="TripExpenses" component={TripExpensesScreen} />
         <Stack.Screen options={{ headerShown: false}} name="AddTripExpenses" component={AddExpenseScreen} />
