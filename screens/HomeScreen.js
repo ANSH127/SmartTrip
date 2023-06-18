54da91a6fd6b83b5072474a100718c5f777e7ab0
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-18 19:13:21 +0530

    designing homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,96 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '../components/screenWrapper'
+import { colors } from '../theme'
+import randomImage from '../assets/images/randomImage'
+
+
+const items = [
+  {
+    id: 1,
+    place: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 2,
+    place: 'Gujrat',
+    country: 'India',
+  },
+  {
+    id: 3,
+    place: 'New York',
+    country: 'United States',
+  },
+  {
+    id: 4,
+    place: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 5,
+    place: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 6,
+    place: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 7,
+    place: 'London',
+    country: 'United Kingdom',
+  },
+  
+]
 
 export default function HomeScreen() {
   return (
-    <View  className='mt-10'>
-      <Text>HomeScreen</Text>
+    <ScreenWrapper className='flex-1'>
+      <View className="flex-row justify-between items-center p-4">
+        <Text className={`${colors.heading} font-bold text-3xl shadow-sm`} >Expensify</Text>
+        <TouchableOpacity className='p-2 px-3 bg-white border border-gray-200 rounded-full' >
+          <Text className={colors.heading} >Logout</Text>
+        </TouchableOpacity>
 
-      
-    </View>
+      </View>
+      <View className='flex-row justify-center items-center bg-blue-200 rounded-xl mx-4 mb-4 ' >
+        <Image source={require('../assets/images/banner.png')} className="w-60 h-60" />
+      </View>
+      <View className='px-4 space-y-4'>
+        <View className="flex-row justify-between items-center">
+          <Text className={`${colors.heading} font-bold text-xl`} >Recent Trips</Text>
+          <TouchableOpacity className='p-2 px-3 bg-white border border-gray-200 rounded-full' >
+            <Text className={colors.heading} >Add Trip</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{height:350}}>
+          <FlatList
+            data={items}
+            numColumns={2}
+            keyExtractor={item => item.id}
+            // showsVerticalScrollIndicator={false}
+            columnWrapperStyle={{ justifyContent: 'space-between' }}
+
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity className="bg-white p-2 rounded-2xl mb-3 shadow-sm" >
+                  <View>
+                    <Image source={randomImage()} className="w-36 h-36 mb-2 " />
+                    <Text className={`${colors.heading} font-bold `} >{item.place}</Text>
+                    <Text className={`${colors.heading} font-bold text-xs `}>{item.country}</Text>
+                  </View>
+                </TouchableOpacity>
+              )
+
+            }}
+
+
+
+
+          />
+        </View>
+      </View>
+    </ScreenWrapper>
   )
 }
---
+++ theme/index.js
@@ -0,0 +1,4 @@
+export const colors = {
+    heading: 'text-gray-700',
+    button: '#50C878'
+}
