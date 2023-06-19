0f2df95c113996cbe13688d09b38f5b5429e8573
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-19 12:10:36 +0530

    connecting addtripscreen with backend

--- screens/AddTripScreen.js
+++ screens/AddTripScreen.js
@@ -4,17 +4,33 @@ import ScreenWrapper from '../components/screenWrapper'
 import { colors } from '../theme'
 import BackButton from '../components/backButton'
 import { useNavigation } from '@react-navigation/native'
+import Loading from '../components/loading'
+import { addDoc } from 'firebase/firestore'
+import { tripsRef } from '../config/firebase'
+import { useSelector } from 'react-redux'
 
 export default function AddTripScreen() {
-    const [place, setPlace] = React.useState('')
-    const [country, setCountry] = React.useState('')
+    const [place, setPlace] = React.useState('');
+    const [country, setCountry] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+    const {user}=useSelector(state=>state.user);
 
     const navigation = useNavigation();
 
-    const handleAddTrip = () => {
+    const handleAddTrip = async() => {
         if(place && country) {
-            console.log(place, country)
-            navigation.navigate('Home')
+            // console.log(place, country)
+            // navigation.navigate('Home')
+            setLoading(true)
+            let doc=await addDoc(tripsRef, {
+                place,
+                country,
+                userId:user.uid
+            });
+            setLoading(false)
+            if(doc && doc.id){
+                navigation.goBack();
+            }
         }
         else{
             Alert.alert('Please fill all the fields')
@@ -51,9 +67,13 @@ export default function AddTripScreen() {
                 </View>
 
                 <View>
+                    {
+                        loading? (<Loading />) : (
+                    
                     <TouchableOpacity style={{backgroundColor:colors.button}} className=" my-6 rounded-full p-3 shadow-sm mx-2 " onPress={handleAddTrip} >
                         <Text className="text-center text-white text-lg font-bold" >Add Trip</Text>
-                    </TouchableOpacity>
+                    </TouchableOpacity>)
+                    }
                 </View>
 
             </View>
